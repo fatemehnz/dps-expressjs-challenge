1305782b6344e7c42d91f5bdd9017097970d7f3b
fatemehnz <fatemeh.nemati@gmail.com>
2024-06-30 12:39:31 +0200

    Adds project repository

---
+++ src/interfaces/project.interface.ts
@@ -0,0 +1,5 @@
+export default interface ProjectInterface {
+	id: string;
+	name: string;
+	description: string;
+}
---
+++ src/repositories/project.repository.ts
@@ -0,0 +1,37 @@
+import db from '../services/db.service';
+import { v4 as uuidv4 } from 'uuid';
+import ProjectInterface from '../interfaces/project.interface';
+
+function getAll() {
+	return db.query('SELECT * FROM projects') as ProjectInterface[];
+}
+
+function create(name: string, description: string) {
+	const id = uuidv4();
+	db.run(
+		'INSERT INTO projects (id, name, description) VALUES (@id, @name, @description)',
+		{ id, name, description },
+	);
+}
+
+function findById(id: string) {
+	const result = db.query('SELECT * FROM projects WHERE id=@id', { id });
+	if (result.length === 0) {
+		return null;
+	}
+
+	return result[0] as ProjectInterface;
+}
+
+function remove(id: string) {
+	db.run('DELETE FROM projects WHERE id=@id', { id });
+}
+
+function update(name: string, description: string, id: string) {
+	db.run(
+		'UPDATE projects SET name=@name, description=@description WHERE id=@id',
+		{ name, description, id },
+	);
+}
+
+export default { getAll, create, findById, remove, update };
